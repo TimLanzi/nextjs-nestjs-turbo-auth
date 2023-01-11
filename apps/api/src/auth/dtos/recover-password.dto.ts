3c1a08e9ad0388bc1c88bee21b16b368eeccfaa2
Tim Lanzi <trlanzi@gmail.com>
2023-01-11 12:44:40 -0500

    add regex for password

--- apps/api/src/auth/dtos/recover-password.dto.ts
+++ apps/api/src/auth/dtos/recover-password.dto.ts
@@ -1,10 +1,11 @@
 import { z } from "zod";
 import { TOKEN_LENGTH } from 'src/util/generate-verify-token';
+import { PASSWORD_REGEX, PASSWORD_REGEX_ERROR_MESSAGE } from "src/util/regex";
 
 export const RecoverPasswordSchema = z.object({
   token: z.string().length(TOKEN_LENGTH, { message: "Token is either invalid or expired" }),
   email: z.string().trim().email(),
-  password: z.string().trim(),
+  password: z.string().trim().regex(PASSWORD_REGEX, { message: PASSWORD_REGEX_ERROR_MESSAGE }),
 });
 
 export type RecoverPasswordDto = z.infer<typeof RecoverPasswordSchema>;
--- apps/api/src/auth/dtos/register.dto.ts
+++ apps/api/src/auth/dtos/register.dto.ts
@@ -1,8 +1,9 @@
+import { PASSWORD_REGEX, PASSWORD_REGEX_ERROR_MESSAGE } from "src/util/regex";
 import { z } from "zod";
 
 export const RegisterSchema = z.object({
   email: z.string().trim().email(),
-  password: z.string().trim(),
+  password: z.string().trim().regex(PASSWORD_REGEX, { message: PASSWORD_REGEX_ERROR_MESSAGE }),
 });
 
 export type RegisterDto = z.infer<typeof RegisterSchema>;
